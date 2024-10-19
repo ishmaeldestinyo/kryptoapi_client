87c4c8339b6db7ae91fd8949dfd5b93870d9c526
Ishmael Destiny Onwe <theimpactindex@gmail.com>
2024-10-20 00:36:45 +0100

    added first commit

---
+++ src/app/layout.js
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { StrictMode } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Import toast styles
+
+export const metadata = {
+  title: "Kryptoapi || Next-Level Security for All Digital Assets",
+  description: "Next-Level Security for All Digital Assets",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <StrictMode>
+          {children}
+          <ToastContainer />
+        </StrictMode>
+      </body>
+    </html>
+  );
+}
